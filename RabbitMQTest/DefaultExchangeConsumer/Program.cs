39583f1c0287c88eaa83a58633be91635fde31f5
IrinaButyugova <irina.butyugova@sharp-dev.net>
2023-10-18 14:15:27 +0300

    DefaultExchange

---
+++ RabbitMQTest/DefaultExchangeConsumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultExchangeConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("dev-queue", false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine($"Receive message: {message}");
+                };
+
+                channel.BasicConsume("dev-queue", true, consumer);
+
+                Console.WriteLine($"Subscribed to the queue 'dev-queue'");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- RabbitMQTest/RabbitMQTest/Program.cs
+++ RabbitMQTest/RabbitMQTest/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace DefaultExchangeProducer
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+
+            while(true)
+            {
+                int timeToSleep = new Random().Next(1000, 3000);
+                Thread.Sleep(timeToSleep);
+
+                var factory = new ConnectionFactory()
+                {
+                    HostName = "localhost"
+                };
+
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare("dev-queue", false, false, false, null);
+
+                        var message = $"Message from publisher N {counter}";
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish("", "dev-queue", body: body);
+
+                        Console.WriteLine($"Message is sent to default Exchange {counter++}");
+                    }
+                }
+            }
+        }
+    }
+}
